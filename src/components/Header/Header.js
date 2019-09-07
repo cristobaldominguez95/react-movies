ecba005e3279b4ee3fe83f91abab0bf8f7c63e0a
Cristóbal Domínguez <cristobaldominguez95@gmail.com>
2019-09-08 01:55:21 +0200

    Add Header and Spinner components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
+      <div className="navbar-item has-text-white">
+        ReactMovies
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
